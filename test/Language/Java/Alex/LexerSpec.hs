044fe43d93847c6ed7218d2f4e39c41b593c8b5b
Javran Cheng <Javran.C@gmail.com>
2021-03-14 20:23:59 -0700

    some test coverage.

--- test/Language/Java/Alex/LexerSpec.hs
+++ test/Language/Java/Alex/LexerSpec.hs
@@ -8,6 +8,7 @@ module Language.Java.Alex.LexerSpec where
  -}
 
 import qualified Data.ByteString.Lazy as BSL
+import Data.Either
 import Language.Java.Alex.Token
 import Language.Java.Alex.Wrapper
 import Test.Hspec
@@ -17,10 +18,25 @@ parseOk raw expected =
   parseAll raw `shouldBe` Right expected
 
 spec :: Spec
-spec =
+spec = do
   describe "Boolean and Null literals" $ do
     specify "NullLiteral" $
       parseOk "null" [NullLiteral]
 
     specify "BooleanLiteral" $
       parseOk "true false" $ BooleanLiteral <$> [True, False]
+
+  describe "IntegerLiteral" $
+    describe "DecimalIntegerLiteral" $ do
+      specify "0" $
+        parseOk "  0  " [Todo "0"]
+      specify "non-zero single digit" $ do
+        parseOk "1" [Todo "1"]
+        parseOk "7" [Todo "7"]
+      specify "many digits" $ do
+        parseOk "90" [Todo "90"]
+        parseOk "12345" [Todo "12345"]
+      specify "interleave with underscores" $ do
+        parseOk "1__2__34__5" [Todo "1__2__34__5"]
+        parseOk "12__34__5" [Todo "12__34__5"]
+        parseAll "_12__34__5" `shouldSatisfy` isLeft
