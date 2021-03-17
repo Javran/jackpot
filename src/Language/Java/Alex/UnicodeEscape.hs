ec5359baa7d8dc7833be8604aad05f9397637479
Javran Cheng <Javran.C@gmail.com>
2021-03-17 00:36:30 -0700

    tests on UnicodeEscape.

--- src/Language/Java/Alex/UnicodeEscape.hs
+++ src/Language/Java/Alex/UnicodeEscape.hs
@@ -1,8 +1,6 @@
 module Language.Java.Alex.UnicodeEscape where
 
 {-
-  TODO: impl.
-
   This technically has nothing to do with Alex,
   but Java spec demands (3.2. Lexical Translations) that Unicode escape to be performed as
   the first step of lexical analysis, which is easier to do outside of Alex's
@@ -14,13 +12,13 @@ module Language.Java.Alex.UnicodeEscape where
   for \u0000 ~ \uFFFF, we just need to map them to corresponding
   chars, and we don't need to deal with surrogate pairs, which simplifies
   preprocessing.
-
  -}
 
 import Control.Monad
 import Data.Char
 import Numeric
 import Text.ParserCombinators.ReadP
+
 
 unicodeEscapeP :: ReadP String
 unicodeEscapeP =
---
+++ test/Language/Java/Alex/UnicodeEscapeSpec.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Language.Java.Alex.UnicodeEscapeSpec where
+
+import Data.Char
+import Language.Java.Alex.UnicodeEscape
+import Test.Hspec
+import Text.RawString.QQ
+
+spec :: Spec
+spec =
+  describe "unicodeEscape" $ do
+    let parseOk inp expected = unicodeEscape inp `shouldBe` Just expected
+        parseSame s = parseOk s s
+        parseFail inp = unicodeEscape inp `shouldBe` Nothing
+    specify "normal" $ do
+      parseSame "AABBB"
+      parseSame "AA😹😹 "
+    specify "simple escape" $ do
+      parseOk [r|\u00AA|] [chr 0x00AA]
+      parseOk [r|\uAbCD|] [chr 0xABCD]
+      parseOk [r|\u6655E|] [chr 0x6655, 'E']
+    specify "many 'u's" $ do
+      parseOk [r|\uuuuuuuuu00AA|] [chr 0x00AA]
+      parseOk [r|\uuuuuuuuuuu1234|] [chr 0x1234]
+    specify "odd slashes are eligible" $ do
+      parseOk [r|\\\\\u1234|] $ [r|\\\\|] <> [chr 0x1234]
+      parseOk [r|AAA123\\\\\u12345|] $ [r|AAA123\\\\|] <> [chr 0x1234] <> "5"
+    specify "even slashes are not eligible" $ do
+      parseSame [r|\\u1234|]
+      parseSame [r|A\\\\u1234B|]
+    specify "odd slashes but no code" $
+      parseSame [r|\\\\\1234|]
+    specify "interleaved" $ do
+      parseOk
+        [r|!!!$\u0123\\uFFEEsss\\\uuuAAFFD|]
+        $ [r|!!!$|] <> [chr 0x0123] <> [r|\\uFFEEsss\\|] <> [chr 0xAAFF] <> "D"
+      parseOk
+        [r|\\\u0123\\\\uu!sss\\\uuuAAFFD|]
+        $ [r|\\|] <> [chr 0x0123] <> [r|\\\\uu!sss\\|] <> [chr 0xAAFF] <> "D"
+    specify "should fail" $ do
+      parseFail [r|\uu|]
+      parseFail [r|\\\uuuu|]
+      parseFail [r|AAA\uABG1|]
+      parseFail [r|\uABCD\uAAVE|]
