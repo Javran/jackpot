c19c4bf844617c97df1c1dd17d58c299d505f97b
Javran Cheng <Javran.C@gmail.com>
2021-03-19 22:46:01 -0700

    put regex generating logic also in code.

--- exe/util/Main.hs
+++ exe/util/Main.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Data.Char
 import System.Environment
+import Text.Replace
 
 javaKeywords :: [String]
 javaKeywords =
@@ -75,9 +77,33 @@ mainGenKeywords = mapM_ go javaKeywords
   where
     go kwRaw = putStrLn $ " , (" <> show kwRaw <> ", " <> toTokenAlt kwRaw <> ")"
 
+mainGenRegex :: IO ()
+mainGenRegex = do
+  putStrLn "TextBlock:"
+  putStr "  "
+  putStrLn $
+    replaceWithList
+      [ Replace "q" "\\\""
+      , Replace "s" "\\\\"
+      , Replace "z" "$NotDbQuoteNotSlash"
+      , Replace "+" "|"
+      ]
+      "qqq(z+s(q+s+z)+q(z+s(q+s+z)+q(z+s(q+s+z))))*qqq"
+  putStrLn "TraditionalComment:"
+  putStr "  "
+  putStrLn $
+    replaceWithList
+      [ Replace "a" "\\/"
+      , Replace "b" "\\*"
+      , Replace "c" "$NotStarNotSlash"
+      , Replace "+" "|"
+      ]
+      "ab(c+a+bb*c)*bb*a"
+
 main :: IO ()
 main =
   getArgs >>= \case
     "tokenalt" : _ -> mainGenToken
     "kw" : _ -> mainGenKeywords
+    "regex" : _ -> mainGenRegex
     _ -> error "unsupported command"
