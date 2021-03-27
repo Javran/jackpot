53634a4945b70c25869b6adab8bda9959f86eefc
Javran Cheng <Javran.C@gmail.com>
2021-03-26 20:09:54 -0700

    test platform functions by example.
    we don't really need a completeness check - that's the job for general category.

--- test/Language/Java/Lexical/PlatformFunctionSpec.hs
+++ test/Language/Java/Lexical/PlatformFunctionSpec.hs
@@ -2,19 +2,19 @@ module Language.Java.Lexical.PlatformFunctionSpec where
 
 import Language.Java.Lexical.PlatformFunction
 import Test.Hspec
+import Control.Monad
 
 spec :: Spec
 spec = do
-  let allChars :: [Char]
-      allChars = [minBound .. maxBound]
-  {-
-    TODO: it is the job of unicode-general-category to check its completeness,
-    rather than this package. Instead we should just have some examples
-    instead of trying to have a complete coverage over all codepoints.
-   -}
-  describe "isJavaIdentifierStart" $
-    specify "length check" $
-      length (filter isJavaIdentifierStart allChars) `shouldBe` 131549
-  describe "isJavaIdentifierPart" $
-    specify "length check" $
-      length (filter isJavaIdentifierPart allChars) `shouldBe` 134698
+  describe "isJavaIdentifierStart" $ do
+    specify "examples"$ do
+      forM_ "a_\xB86\x1CEF" $ \ch ->
+        isJavaIdentifierStart ch `shouldBe` True
+      forM_ "6\x09D7" $ \ch ->
+        isJavaIdentifierStart ch `shouldBe` False
+  describe "isJavaIdentifierPart" $ do
+    specify "examples"$ do
+      forM_ "a_6\x09D7\xB86\xD4C" $ \ch ->
+        isJavaIdentifierPart ch `shouldBe` True
+      forM_ "\xD49\x201F" $ \ch ->
+        isJavaIdentifierPart ch `shouldBe` False
